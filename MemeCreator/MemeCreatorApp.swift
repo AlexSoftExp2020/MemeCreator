56d5dc30e1544da3261cd23d4e71f4fe66fd5a2b
Alex <oleksii.pidhornyi@gmail.com>
2025-01-22 20:07:11 +0100

    Initial Commit

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCreatorApp.swift
+//  MemeCreator
+//
+//  Created by Oleksii on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
